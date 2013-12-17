7cb89de2b78bf0582e83f224cebde8d047ac2ed3
Marcian Diamond <marciandmnd@yahoo.com>
2013-12-16 17:10:40 -0800

    yea

--- index.php
+++ index.php
@@ -2,46 +2,40 @@
     <head>
         <title>T3MPO - Touchless 3D Music Production Overlord</title>    
         <link rel ="stylesheet" type ="text/css" href ="css/bootstrap.css">
-        <link rel ="stylesheet" type ="text/css" href ="css/bootstrap-responsive.css">
         <link rel ="stylesheet" type ="text/css" href ="css/custom.css">
+        <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Iceland|Play|Droid+Sans">
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" type="image/png" href="images/logo.png">
     </head>
     <body> 
-			<nav id = "navigation" class="navbar navbar-default" role="navigation" style="width:100%">
-			<!-- Brand and toggle get grouped for better mobile display -->
+			<nav id = "navigation" class="navbar-static-top" style ="position:fixed;white-space: nowrap;overflow: hidden;" role="navigation" width="100%">
 				<div class="navbar-header">
-					<img class = "navbar-brand" src= "images/logo.png" height="75px">
-					<a class="navbar-brand" href="index.php" id="homeLink" style="padding-top:30px;font-size:25px;"><strong>T3MPO: </strong>Touchless 3-Dimensional Music Production Overlord</a>
+					<a href = "index.php"> <img class = "navbar-brand" src= "images/logo.png" height="75px"></a>
 				</div>
-				<!-- Collect the nav links, forms, and other content for toggling -->
-				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-					<ul class="nav navbar-nav">
-						<li class=""><a href="#"></a></li>
-					</ul>
-					
-				</div><!-- /.navbar-collapse -->
+				<div class = "title" style="padding-top:20px;">
+					<a href="index.php";id="homeLink" style="font-family:'Iceland',serif;font-size:30px;"><strong>T3MPO: </strong>Touchless 3-Dimensional Music Production Overlord</a>
+				</div>
 			</nav>
-         	<div class ="col-md-3" style="padding-top:30px; padding-bottom:50px; float:left;"> 
-				<ul id = "pills" class="nav nav-pills nav-stacked" style="border:2px solid;border-radius:25px; width:250px; position:fixed;">
-					<li name = 'desc'><a href="#" id="descriptionLink">I. Description</a></li>
-					<li name = 'intro'><a href="#" id="introLink">II. Introduction</a></li>
-					<li name = 'bg'><a href="#" id="bgLink">III. Background</a></li>
-					<li name = 'cons'><a href="#" id="consLink">IV. Design Considerations</a></li>
-					<li name = 'specs'><a href="#" id="specsLink">V. Design Specs, Alternatives, & Implementation</a></li>
-					<li name = 'sched'><a href="#" id="schedLink">VI. Schedule and Budget</a></li>
-					<li name = 'results'><a href="#" id="resultsLink">VII. Results</a></li>
-					<li name = 'disc'><a href="#" id="discussionLink">VIII. Discussion & Conclusion</a></li>
-					<li name = 'team'><a href="#" id="teamLink">IV. Team</a></li>
-				</ul>
-			</div>
-			<div class="col-md-9" id="mainContent" style="position:absolute;margin-left:300px;padding-right:100px; padding-top:30px;padding-bottom:100px;">
-			</div>
-            <nav class="navbar navbar-inverse navbar-fixed-bottom" style="position:fixed;"role="navigation">
-                <div class ="footer">
-                    SFSU ENGR 697: Engineering Design Project II, Fall 2013 
-                </div>
-            </nav>
+     	<div class ="col-md-3" style="padding-top:50px; padding-bottom:50px; padding-left:40px;"> 
+			<ul id = "pills" class="nav nav-pills nav-stacked" style="border:2px solid;border-radius:25px; width:250px; position:fixed;">
+				<li name = 'desc'><a href="#" id="descriptionLink">I. Description</a></li>
+				<li name = 'intro'><a href="#" id="introLink">II. Introduction</a></li>
+				<li name = 'bg'><a href="#" id="bgLink">III. Background</a></li>
+				<li name = 'cons'><a href="#" id="consLink">IV. Design Considerations</a></li>
+				<li name = 'specs'><a href="#" id="specsLink">V. Design Specs, Alternatives, & Implementation</a></li>
+				<li name = 'sched'><a href="#" id="schedLink">VI. Schedule and Budget</a></li>
+				<li name = 'results'><a href="#" id="resultsLink">VII. Results</a></li>
+				<li name = 'disc'><a href="#" id="discussionLink">VIII. Discussion & Conclusion</a></li>
+				<li name = 'team'><a href="#" id="teamLink">IV. Team</a></li>
+			</ul>
+		</div>
+		<div class="col-md-9" id="mainContent" style="position:absolute;margin-left:350px;padding-right:150px; padding-top:30px;padding-bottom:100px;">
+		</div>
+        <nav class="navbar navbar-inverse navbar-fixed-bottom" style="position:fixed;"role="navigation">
+            <div class ="footer">
+                SFSU ENGR 697: Engineering Design Project II, Fall 2013 
+            </div>
+        </nav>
     	<script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
     	<script src="js/bootstrap.min.js"></script>
     	<script src="js/script.js"></script>
